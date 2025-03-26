3514a123961913fba6c33d76335a813c2d185dc9
Urrightnut <kriisalu1@gmail.com>
2025-03-27 00:07:08 +0200

    Update Pangakonto_haldur.py

--- Pangakonto_haldur.py
+++ Pangakonto_haldur.py
@@ -1,25 +1,39 @@
 def konto_haldur(algne_saldo, toiming, summa):
-    """
-    Funktsioon pangakonto haldamiseks.
-    
-    :param algne_saldo: Konto algne saldo.
-    :param toiming: 'deposiit' raha lisamiseks või 'valjavote' raha eemaldamiseks.
-    :param summa: Summa, mida soovitakse lisada või eemaldada.
-    :return: Uus saldo pärast toimingut.
-    """
-    if toiming == "deposiit":
+    if toiming == 'deposiit':
         algne_saldo += summa
-    elif toiming == "valjavote":
+    elif toiming == 'valjavote':
         if summa > algne_saldo:
-            return "Viga: Ebapiisav saldo!"
+            return "Viga: Ebapiisav saldo"
         algne_saldo -= summa
     else:
-        return "Viga: Tundmatu toiming!"
+        return "Viga: Tundmatu toiming"
     
     return algne_saldo
 
-# Näidised
-print(konto_haldur(100, "deposiit", 50))   # Väljund: 150
-print(konto_haldur(150, "valjavote", 20))  # Väljund: 130
-print(konto_haldur(130, "valjavote", 200)) # Väljund: Viga: Ebapiisav saldo!
-print(konto_haldur(130, "vale_toiming", 10)) # Väljund: Viga: Tundmatu toiming!
+if __name__ == "__main__":
+    algne_saldo = float(input("Sisesta algne saldo: "))
+    while True:
+        print("\nVali toiming:")
+        print("1 - Deposiit")
+        print("2 - Väljavõte")
+        print("3 - Lõpeta")
+        valik = input("Sisesta valiku number: ")
+        
+        if valik == '3':
+            print("Nägemist!")
+            break
+        elif valik == '1':
+            toiming = 'deposiit'
+        elif valik == '2':
+            toiming = 'valjavote'
+        else:
+            print("Viga: Vigane valik")
+            continue
+        
+        summa = float(input("Sisesta summa: "))
+        uus_saldo = konto_haldur(algne_saldo, toiming, summa)
+        if isinstance(uus_saldo, str):
+            print(uus_saldo)
+        else:
+            algne_saldo = uus_saldo
+            print(f"Uus saldo: {algne_saldo}")
